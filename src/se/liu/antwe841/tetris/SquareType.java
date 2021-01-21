c8c2e50e467fd52e4ca3794b3f4620d134fc8605
Anton <antwe841@student.liu.se>
2021-01-21 16:58:35 +0100

    Began working in upg. 2

---
+++ src/se/liu/antwe841/tetris/SquareType.java
@@ -0,0 +1,21 @@
+package se.liu.antwe841.tetris;
+
+
+import java.sql.Array;
+import java.util.Random;
+
+public enum SquareType
+{
+    EMPTY, I, O, T, S, Z, J, L;
+
+    public static void main(String[] args) {
+	Random rnd = new Random();
+	SquareType[] blockArray = SquareType.values();
+
+	for (int i = 0; i < 25; i++) {
+	    SquareType currentBlock = blockArray[rnd.nextInt(blockArray.length)];
+	    System.out.println(currentBlock);
+	}
+    }
+}
+
